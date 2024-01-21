f7a228e4606e7ac76679ef2d298497beddbff82d
HenryVoid <azicplus@naver.com>
2024-01-21 13:10:44 +0900

    NetworkKit init

---
+++ Sources/NetworkKit/NetworkError.swift
@@ -0,0 +1,29 @@
+
+import Foundation
+
+enum NetworkError: LocalizedError {
+    case failConnection
+    case invalidURL
+    case invalidStatusCode(code: Int)
+    case emptyData
+    case decodingError
+    case responseError
+    case unknownError(message: String)
+    
+    var errorDescription: String? {
+        switch self {
+        case .failConnection:
+            return "네트워크 연결이 불안정해요"
+        case .invalidURL:
+            return "유효하지 않은 URL이에요"
+        case .invalidStatusCode, .responseError:
+            return "나중에 다시 시도해주세요"
+        case .emptyData:
+            return "유효하지 않은 결과에요"
+        case .decodingError:
+            return "알 수 없는 에러가 발생했어요"
+        case .unknownError(let message):
+            return message
+        }
+    }
+}
---
+++ Sources/NetworkKit/NetworkProvider.swift
@@ -0,0 +1,59 @@
+
+import Foundation
+
+protocol NetworkRequestable {
+    func data(for urlRequest: URLRequest) async throws -> (Data, URLResponse)
+}
+
+extension URLSession: NetworkRequestable {}
+
+protocol NetworkProviderType: AnyObject {
+    @discardableResult
+    func request<T: Decodable>(endpoint: EndpointType, type: T.Type) async throws -> T
+}
+
+final class NetworkProvider: NetworkProviderType {
+    
+    // MARK: Properties
+    private let session: NetworkRequestable
+    
+    // MARK: Initializer
+    init(session: URLSession = .shared) {
+        self.session = session
+    }
+    
+    // MARK: Methods
+    @discardableResult
+    func request<T: Decodable>(endpoint: EndpointType, type: T.Type) async throws -> T {
+        guard NetworkMonitor.shared.isConnected else {
+            throw NetworkError.failConnection
+        }
+        
+        let request: URLRequest = try endpoint.makeURLRequest()
+        let (data, response) = try await session.data(for: request)
+        try filterNetworkingError(data: data, response: response)
+        
+        guard let decoded = try? JSONDecoder().decode(T.self, from: data) else {
+            throw NetworkError.decodingError
+        }
+        
+        return decoded
+    }
+}
+
+// MARK: - Private
+private extension NetworkProvider {
+    func filterNetworkingError(data: Data, response: URLResponse) throws {
+        guard let httpResponse = response as? HTTPURLResponse else {
+            throw NetworkError.responseError
+        }
+        
+        guard 200...299 ~= httpResponse.statusCode else {
+            throw NetworkError.invalidStatusCode(code: httpResponse.statusCode)
+        }
+        
+        guard !data.isEmpty else {
+            throw NetworkError.emptyData
+        }
+    }
+}
