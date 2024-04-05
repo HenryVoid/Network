396c65991e396a9b57de42f1913afbd1a35dd2b2
HenryVoid <azicplus@naver.com>
2024-04-05 19:59:05 +0900

    add APIProtocol

---
+++ Sources/NetworkKit/APIProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  APIProtocol.swift
+//
+//
+//  Created by 송형욱 on 4/5/24.
+//
+
+import Foundation
+
+protocol APIProtocol {
+    func request<T: Decodable>(_ endpoint: Endpoint, decode: T.Type, repeatCount: Int) async throws -> T
+}
