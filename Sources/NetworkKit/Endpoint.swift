fd816b67dd7c129d1c3a4d62bbfbf633e05cde88
woogi <azicplus@naver.com>
2024-11-15 17:31:21 +0900

    add Common Endpoint Property

--- Sources/NetworkKit/Endpoint.swift
+++ Sources/NetworkKit/Endpoint.swift
@@ -21,4 +21,60 @@ protocol Endpoint: URLRequestConvertible {
 
 extension Endpoint {
     // TODO: Common Endpoint property
+    var baseURL: String { "" }
+    
+    private var defaultHeaders: HTTPHeaders {
+        var headers: HTTPHeaders = [
+            .accept("application/json"),
+            .contentType("application/json")
+        ]
+        
+        if let token = token {
+            headers.add(.authorization(bearerToken: token))
+        }
+        
+        return headers
+    }
+    
+    public func asURLRequest() throws -> URLRequest {
+        guard let url = URL(string: baseURL)?.appendingPathComponent(path) else {
+            throw APIError(
+                errorCode: "ERROR",
+                message: "Invalid baseURL or path"
+            )
+        }
+        
+        var components = URLComponents(url: url, resolvingAgainstBaseURL: false)
+        
+        // Query
+        if let queryItems = queryItems {
+            components?.queryItems = queryItems.map { URLQueryItem(name: $0.name, value: $0.value) }
+        }
+        
+        guard let finalURL = components?.url else {
+            throw APIError(
+                errorCode: "ERROR",
+                message: "Invalid URL Component"
+            )
+        }
+        
+        var request = URLRequest(url: finalURL)
+        // Method
+        request.method = method
+        
+        // Header
+        var finalHeader = defaultHeaders
+        if let customHeaders = headers {
+            customHeaders.forEach { finalHeader.update($0) }
+        }
+        
+        request.headers = finalHeader
+        
+        // Body
+        if let body = body {
+            request.httpBody = try? JSONSerialization.data(withJSONObject: body)
+        }
+        
+        return request
+    }
 }
