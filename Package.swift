32d9b5f585ff2ae1c0955fb64494bf85c9595803
woogi <azicplus@naver.com>
2025-01-18 16:37:25 +0900

    Session 재정의

--- Package.swift
+++ Package.swift
@@ -9,23 +9,15 @@ let package = Package(
         .iOS(.v17)
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "NetworkKit",
             targets: ["NetworkKit"]
         ),
-    ], 
-    dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.9.1"))
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "NetworkKit",
-            dependencies: [
-                "Alamofire"
-            ]
+            dependencies: []
         ),
         .testTarget(
             name: "NetworkTests",
---
+++ Sources/NetworkKit/API+Session.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  NetworkKit
+//
+//  Created by 송형욱 on 1/18/25.
+//
+
+import Foundation
+
+#if !os(macOS)
+extension API {
+    protocol Session {
+        func data(for request: URLRequest) async throws -> (Data, URLResponse)
+    }
+}
+
+extension URLSession: API.Session {}
+#endif
