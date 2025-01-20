2acd0ad25598f536411271ea0508bc0316cf7d5e
woogi <azicplus@naver.com>
2025-01-20 17:04:10 +0900

    JsonEncodingTests 추가

---
+++ Tests/NetworkTests/JSONEncodingTests.swift
@@ -0,0 +1,31 @@
+import Testing
+import Foundation
+
+@testable import NetworkKit
+
+#if !os(macOS)
+@MainActor
+struct JSONEncodingTests {
+    
+    @Test
+    func encode_성공() async throws {
+        let encoder: any API.ParameterEncodable = API.JSONParameterEncoder()
+        let urlRequest: URLRequest = .init(url: .init(string: "https://www.naver.com")!)
+        let parameters: [String: Sendable & Any] = [
+          "name": "henry",
+          "array": ["a", 1, true] as [Sendable & Any],
+          "object": [
+            "a": 1,
+            "b": [2, 2],
+            "c": ["3", "3", "3"]
+          ] as [String: Sendable & Any]
+        ]
+        
+        let result = try encoder.encode(request: urlRequest, with: parameters)
+        
+        #expect(result.httpBody != nil)
+        #expect(try result.httpBody?.asJSONObject() as? NSObject == parameters as NSObject)
+    }
+
+}
+#endif
